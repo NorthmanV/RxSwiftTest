06dc4d8a774c7cd0dad56302aec875e9da43865d
Ruslan Akberov <ruslanakberov@gmail.com>
2018-10-16 23:15:22 +0300

    Initial Commit

---
+++ RxSwiftTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RxSwiftTest
+//
+//  Created by Руслан Акберов on 16/10/2018.
+//  Copyright © 2018 Ruslan Akberov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
